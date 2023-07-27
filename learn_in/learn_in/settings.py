347f0e4b5bb5ac860fd17b0bd1933e08be7deb04
Алексей Шульга <shulga.alexey.00@yandex.ru>
2023-07-28 01:32:33 +0300

    Fix settings.

--- learn_in/learn_in/settings.py
+++ learn_in/learn_in/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'tasks',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# User model
+
+AUTH_USER_MODEL = 'users.User'
