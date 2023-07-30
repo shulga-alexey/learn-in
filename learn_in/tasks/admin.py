9928c07eafda5e77e449d99648530943d98252b6
Алексей Шульга <shulga.alexey.00@yandex.ru>
2023-07-30 13:01:03 +0300

    Update tasks admin.

--- learn_in/tasks/admin.py
+++ learn_in/tasks/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from .models import Task
+from .models import Task, TaskGroup
 
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
+
     list_display = ('title', 'producer_teacher', 'status', 'get_task_groups')
     list_filter = ('producer_teacher', 'status')
     search_fields = ('title',)
@@ -13,3 +14,12 @@ class TaskAdmin(admin.ModelAdmin):
     @admin.display(description='группы задач')
     def get_task_groups(self, obj):
         return ',\n'.join(item.title for item in obj.task_groups.all())
+
+
+@admin.register(TaskGroup)
+class TaskGroupAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'slug', 'created')
+    search_fields = ('title', 'slug')
+    ordering = ('title',)
+    empty_value_display = '-empty-'
