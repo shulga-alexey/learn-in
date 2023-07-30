d041c73d0de17688d653bea4c3f9cc166f9e41a9
Алексей Шульга <shulga.alexey.00@yandex.ru>
2023-07-30 12:55:03 +0300

    Update settings.

--- learn_in/learn_in/settings.py
+++ learn_in/learn_in/settings.py
@@ -57,10 +57,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'learn_in.urls'
 
+TEMPLATES_DIR = BASE_DIR / 'templates'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
